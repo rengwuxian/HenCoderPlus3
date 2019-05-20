59903527c1901d33a52a5917833208b8d29a0801
Kai Zhu <rengwuxian@gmail.com>
2019-05-20 09:19:13 +0800

    20 drag and nested scroll

---
+++ 20_drag_nestedscroll/src/main/java/com/hencoder/a20_drag_nestedscroll/MainActivity.java
@@ -0,0 +1,14 @@
+package com.hencoder.a20_drag_nestedscroll;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.nested_scalable_image_view);
+    }
+}
