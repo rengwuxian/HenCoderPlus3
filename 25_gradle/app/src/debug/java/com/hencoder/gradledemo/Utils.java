69add10ad3cb591fb4904100eafad78ec5f114f8
Kai Zhu <rengwuxian@gmail.com>
2019-06-04 22:48:30 +0800

    25 gradle

---
+++ 25_gradle/app/src/debug/java/com/hencoder/gradledemo/Utils.java
@@ -0,0 +1,15 @@
+package com.hencoder.gradledemo;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Utils {
+    public static void drawBadge(Activity activity) {
+        ViewGroup decorView = (ViewGroup) activity.getWindow().getDecorView();
+        View badge = new View(activity);
+        badge.setBackgroundColor(Color.GREEN);
+        decorView.addView(badge, 200, 100);
+    }
+}
